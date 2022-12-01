1cd0bf6db01b654369181d683dc98bf7e82b3144
Heiko Dotzauer <heiko.dotzauer@t-systems.com>
2022-12-01 14:06:15 +0100

    restored ETL Pipeline notebook

--- disaster_response_pipeline_project/app/run.py
+++ disaster_response_pipeline_project/app/run.py
@@ -27,7 +27,7 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/myResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('CleanedData', engine)
 
 # load model
